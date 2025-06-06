6d9ada507d9ba19582a2f39105fd37356142c122
Sutirtha Dey <sutirth.dey@prediqt.it>
2025-06-06 20:04:35 +0530

    message divs added

--- src/features/Chat/Conversation.jsx
+++ src/features/Chat/Conversation.jsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 import { useOutletContext } from "react-router-dom";
 
@@ -5,9 +6,25 @@ const Conversation = () => {
   const { chatHistory } = useOutletContext();
   console.log(chatHistory);
   return (
-    <div>
+    <div className="flex flex-col gap-10">
       {chatHistory?.map((conv) => (
-        <p key={conv.id}>{conv.message}</p>
+        <div
+          className={clsx(
+            "flex",
+            conv.role === "user" ? "justify-end" : "justify-start"
+          )}
+        >
+          <div
+            className={clsx(
+              "w-fit px-3 py-2 break-words rounded-2xl",
+              conv.role === "user"
+                ? "max-w-[33%] bg-[#636AE81A]  rounded-br-none"
+                : "max-w-[100%] border-2 border-solid border-[#F3F4F6FF] rounded-tl-none"
+            )}
+          >
+            <p>{conv.message}</p>
+          </div>
+        </div>
       ))}
     </div>
   );
