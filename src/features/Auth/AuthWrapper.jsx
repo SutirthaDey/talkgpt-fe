3df73903fa365929026d146de6e6688601cc1961
Sutirtha Dey <sutirth.dey@prediqt.it>
2025-06-09 05:10:45 +0530

    Health check added

--- src/features/Auth/AuthWrapper.jsx
+++ src/features/Auth/AuthWrapper.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SocialAuth from "./SocialAuth";
 import ManualAuth from "./ManualAuth";
+import { useApiRequest } from "../../hooks/useApiRequest";
 
 const AuthWrapper = ({ type }) => {
+  const apiRequest = useApiRequest();
+
+  useEffect(() => {
+    async function healthCheck() {
+      await apiRequest("health");
+    }
+
+    healthCheck();
+  }, [apiRequest]);
+
   return (
     <div className="h-[500px] w-[550px] bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-2 border-red-400 rounded-xl flex flex-col z-20 text-center">
       <h1 className="text-3xl leading-10 font-Archivo  text-[#171A1FFF] font-bold mt-10">
