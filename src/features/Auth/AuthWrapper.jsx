a6354589dc803a9cce33d218ab0b877ec232ad73
Sutirtha Dey <sutirth.dey@prediqt.it>
2025-06-13 13:51:43 +0530

    sign up page refactored

--- src/features/Auth/AuthWrapper.jsx
+++ src/features/Auth/AuthWrapper.jsx
@@ -19,20 +19,23 @@ const AuthWrapper = ({ type }) => {
   return (
     <>
       {loader && <GlobalWheel text="Redirecting..." />}
-      <div className="h-[500px] w-[550px] bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-2 border-red-400 rounded-xl flex flex-col z-20 text-center">
-        <h1 className="text-3xl leading-10 font-Archivo  text-[#171A1FFF] font-bold mt-10">
+      <div className="w-[90%] sm:w-[500px] md:w-[550px] bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-2 border-red-400 rounded-xl flex flex-col z-20 text-center py-6 px-4 sm:px-6">
+        <h1 className="text-xl sm:text-2xl md:text-3xl leading-snug font-Archivo text-[#171A1FFF] font-bold mt-2 sm:mt-4 md:mt-8">
           {type === "signup"
             ? "Create Your Account"
             : "Sign In to Your Account"}
         </h1>
+
         <SocialAuth loader={loader} setLoader={setLoader} />
-        <div className="mx-auto flex justify-center">
+
+        <div className="mx-auto flex justify-center sm:mb-3">
           <img
             src="/assests/svg/DividerLine.svg"
             alt="ruler"
-            className="w-[350px]"
+            className="w-[95%]"
           />
         </div>
+
         <ManualAuth type={type} loader={loader} setLoader={setLoader} />
       </div>
     </>
--- src/features/Auth/SocialAuth.jsx
+++ src/features/Auth/SocialAuth.jsx
@@ -7,14 +7,8 @@ import { GoogleAuth } from "./GoogleAuth";
 const SocialAuth = ({ loader, setLoader }) => {
   return (
     <div>
-      <div className="w-full flex justify-center items-center mt-5 h-20 gap-3">
+      <div className="w-full flex justify-center items-center mt-4 sm:mt-5 h-auto sm:h-20 gap-2 sm:gap-3 px-4">
         <GoogleAuth loader={loader} setLoader={setLoader} />
-        {/* <button className="h-12 w-12 flex justify-center items-center rounded-full bg-[#335CA6FF] hover:bg-[#233F72FF]">
-          <FaFacebook className="text-white size-5" />
-        </button>
-        <button className="h-12 w-12 flex justify-center items-center rounded-full bg-[#000000] hover:bg-[#353535]">
-          <PiAppleLogoBold className="text-white size-5" />
-        </button> */}
       </div>
     </div>
   );
